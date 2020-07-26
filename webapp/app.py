bb0c9ba20ecb4d0faf54f498b27448faa3e02429
ilee <iram.lee@gmail.com>
2020-07-26 13:53:50 -0500

    add dynamic content

--- webapp/app.py
+++ webapp/app.py
@@ -2,13 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/sim')
 def simulation():
     return render_template('sim.html')
+
+
+@app.route('/hello/<name>')
+def hello(name):
+    return render_template('page.html', name=name)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
